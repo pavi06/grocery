d3f676a7d27423febe5cb78240a9e0be41db7489
pavi06 <pavi06saishivu@gmail.com>
2022-08-05 16:45:11 +0530

    database connected

---
+++ gshop/connection.js
@@ -0,0 +1,17 @@
+const { createPool } = require("mysql")
+
+const pool = createPool({
+    host:"localhost",
+    user:"root",
+    password:"Pavi",
+    database:"gshop",
+    connectionLimit:10
+})
+
+pool.query(`select * from products`, (err,result,fields)=>{
+    if(err){
+        console.log("Error");
+        console.log(err.message);
+    }
+    return console.log(result);
+})
